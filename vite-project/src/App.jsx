6f9aa3d983ac562b721a0fbaa63f66746fcf75b7
Syed-Obaid <syedobaid932@gmail.com>
2023-11-17 08:42:07 -0800

    first commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <h1>hello world</h1>
+     <img src="/vite.svg" alt="" />
+    </>
+  )
+}
+
+export default App
